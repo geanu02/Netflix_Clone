958c8d04c9ef9c8bf603048999be52284dc9618f
Gian Carlo Torres <geanu@outlook.com>
2021-11-02 22:38:12 -0400

    Added ENV functionality

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJ_ID}`,
+    storageBucket: `${process.env.REACT_APP_STOR_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MSGR_SEND_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`
+}
+
+export default firebaseConfig
