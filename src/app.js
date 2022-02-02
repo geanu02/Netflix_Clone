ed8379bc8239fa3317eff9e044eb2761fbf6607c
Gian Carlo Torres <geanu@outlook.com>
2022-02-02 11:32:59 -0500

    Mock Route updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 import { Home, SignIn, SignUp, Browse } from "./pages"
 import { useAuthListener } from './hooks';
@@ -7,14 +7,10 @@ import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export function App() {
     const { user } = useAuthListener()
-    // console.log("User Console: " + JSON.stringify(user))
 
     return (
         <Router>
             <Switch>
-                {/* <Route exact path="/mock">
-                    <Browse />
-                </Route> */}
                 <IsUserRedirect
                     exact
                     path={ROUTES.MOCK}
